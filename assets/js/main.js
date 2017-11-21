6457880d1aa6557780832cb498a4cfd7b52fc948
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2017-11-21 17:44:26 +0200

    loadNextPost()

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,50 @@ jQuery(document).ready(function($) {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
         h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-    
+	$(window).on('load', function(event) {
+		
+		var swiperPosts = new Swiper('#content .loop .swiper-container', {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			centeredSlides: true,
+			autoHeight: true,
+			// simulateTouch: false
+		});
+
+		var currentPage = 1;
+		var pathname = window.location.pathname;
+		var $result = $('#content .loop .swiper-wrapper');
+
+		pathname = pathname.replace(/#(.*)$/g, '').replace('/\//g', '/');
+
+		var nextPage = pathname + 'page/' + currentPage + '/';
+
+        loadNextPost( maxPages, nextPage);
+
+		swiperPosts.on('slideChange', function(event) {
+			console.log(swiperPosts.slides.length);
+			if (swiperPosts.activeIndex < (swiperPosts.slides.length-1)) {
+				loadNextPost( maxPages, nextPage);
+			};
+		});			
+
+		function loadNextPost(maxPages, nextPage){
+			if (currentPage == maxPages) {
+				return;
+			};
+
+			currentPage++;
+
+        	nextPage = pathname + 'page/' + currentPage + '/';
+
+	        $.get(nextPage, function (content) {
+	            $result.append($(content).find('#content .loop .swiper-slide'));
+	            swiperPosts.update(true);
+	        });
+		}
+
+	});
+
+
 
 });
